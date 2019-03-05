3900fb0571da5c397665f809497624885f97d0c8
Stefan Cassar <stefan.cassar@bookingandco.com>
2019-03-05 17:06:02 +0100

    Add empty config shell

---
+++ config/config.js
@@ -0,0 +1,18 @@
+var config = {
+	production: {
+		// google calendar config
+		calendar: {
+			id: ""
+		},
+		// atlassian token details
+		atlassian: {
+			token: ''
+		},
+		//server details
+		server: {
+			host:   '127.0.0.1',
+			port:   '1234'
+		}
+	}
+};
+module.exports = config;
